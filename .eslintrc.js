de45667fadc78b4295df8f3b06bcbc0acc3ac4d5
이지윤 <1yoouoo@gmail.com>
2023-02-23 20:45:16 +0900

    fail to debugging postman request with vscode

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
   extends: ['airbnb-base', 'plugin:node/recommended', 'prettier'],
-}
+  rules: {
+    'node/no-unsupported-features/es-syntax': [
+      'error',
+      { ignores: ['modules'] },
+    ],
+  },
+};
--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,5 @@
-import { BoardsService } from './boards.service'
+import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
+import { BoardsService } from './boards.service';
 import {
   Body,
   Controller,
@@ -9,9 +10,9 @@ import {
   Post,
   UsePipes,
   ValidationPipe,
-} from '@nestjs/common'
-import { Board, BoardStatus } from './boards.model'
-import { CreateBoardDto } from './DTO/create-board.dto'
+} from '@nestjs/common';
+import { Board, BoardStatus } from './boards.model';
+import { CreateBoardDto } from './DTO/create-board.dto';
 
 @Controller('boards')
 export class BoardsController {
@@ -19,26 +20,26 @@ export class BoardsController {
 
   @Get('/')
   getAllBoard(): Board[] {
-    return this.BoardsService.getAllBoards()
+    return this.BoardsService.getAllBoards();
   }
   @Post()
   @UsePipes(ValidationPipe)
   createBoard(@Body() createBoardDto: CreateBoardDto): Board {
-    return this.BoardsService.createBoard(createBoardDto)
+    return this.BoardsService.createBoard(createBoardDto);
   }
   @Get('/:id')
   getBoardById(@Param('id') id: string) {
-    return this.BoardsService.getBoardById(id)
+    return this.BoardsService.getBoardById(id);
   }
   @Delete(':id')
   deleteBoard(@Param('id') id: string): void {
-    this.BoardsService.deleteBoard(id)
+    this.BoardsService.deleteBoard(id);
   }
   @Patch('/:id/status')
   updateBoardStatus(
     @Param('id') id: string,
-    @Body('status') status: BoardStatus
+    @Body('status', BoardStatusValidationPipe) status: BoardStatus
   ) {
-    return this.BoardsService.updateBoardStatus(id, status)
+    return this.BoardsService.updateBoardStatus(id, status);
   }
 }
