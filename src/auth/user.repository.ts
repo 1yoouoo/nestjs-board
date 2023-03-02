b3d87e8dd33cdf4454bc70bdf351a2eeee9339a2
이지윤 <1yoouoo@gmail.com>
2023-03-02 17:43:25 +0900

    feat: throw error when creating already exist username

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,5 +1,9 @@
 import { InjectRepository } from '@nestjs/typeorm';
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { AuthCredentialsDto } from './DTO/auth-credential.dto';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
@@ -14,7 +18,14 @@ export class UserRepository {
   async createUser(authCredentialDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialDto;
     const user = this.db.create({ username, password });
-
-    await this.db.save(user);
+    try {
+      await this.db.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('이미 존재하는 아이디입니다.');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
